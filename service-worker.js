b811160cc0ec75f5bba18f2d889bd43f7389dd0c
LoveOfMyLife1 <jtbsr927@gmail.com>
2026-05-06 05:36:40 -0600

    Refresh planner app shell cache

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,16 @@
-const CACHE_NAME = "master-task-planner-v4";
+const CACHE_NAME = "master-task-planner-v5";
 const APP_SHELL = ["./", "./index.html", "./manifest.json"];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys => Promise.all(
       keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-    ))
+    )).then(() => self.clients.claim())
   );
 });
 
